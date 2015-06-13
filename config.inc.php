8efa403aaca06eedd6d4214a6e7d0bbccc4e94f0
Marc 'Judge' Richter <mail@marc-richter.info>
2015-06-13 16:04:40 +0200

    Testing to re-set BFRAND on every restart

--- config.inc.php
+++ config.inc.php
@@ -3,6 +3,13 @@ $cfg['blowfish_secret'] = 'BFSECRET';
 $cfg['DisplayServersList'] = true;
 $cfg['AllowArbitraryServer'] = true;
 
+/*
+ Sets here the complete URL (with full path) to your phpMyAdmin installation’s directory. E.g.
+ http://www.example.net/path_to_your_phpMyAdmin_directory/. Note also that the URL on most of web servers are
+ case–sensitive. Don’t forget the trailing slash at the end.
+*/
+$cfg['PmaAbsoluteUri'] = 'PMAABSOLUTEURI';
+
 $i = 0;
 $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Generate random Blowfish-secret
+export BFRAND="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)" \
+  && sed -i'' "s#\$cfg\[.*'blowfish_secret'.*\].*=.*'.*'.*\;.*\$#\$cfg[\'blowfish_secret\'] = "\'${BFRAND}\'";#g" \
+    /usr/share/nginx/html/config.inc.php \
+  && unset BFRAND
+
+## Set PmaAbsoluteUri if Env var PMAABSOLUTEURI is set
+#if [ ! -z "${PMAABSOLUTEURI}" ]; then
+#    sed -i''
+#fi
 
 # Start daemons
 php5-fpm --fpm-config /etc/php5/fpm/php-fpm.conf -g /var/run/php5-fpm.pid -D
